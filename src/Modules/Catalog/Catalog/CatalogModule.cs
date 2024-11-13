5f18575c16160ad4e7e1149cd3d6e17f2299991f
Guillermo Arroyo <gmarroyo@yahoo.com>
2024-11-13 13:11:21 -0300

    Se configuro el DbContext para producto, con la configuracion estilo fluent. Se desarrollo un interceptor para realizar la auditoria al momento de guardar cambios en la BD.

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,7 +1,7 @@
-﻿
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Interceptors;
 
 namespace Catalog
 {
@@ -10,12 +10,34 @@ namespace Catalog
         public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
         {
 
+            //Add services to the container.
+
+            //Api Endpoints Services
+
+            //Application Use Case Services
+
+            //Data - Infrastructure Services
+
+            services.AddDbContext<CatalogDbContext>(options =>
+            {          
+                options.AddInterceptors(new AuditableEntityInterceptor());
+                options.UseNpgsql(configuration.GetConnectionString("Database"));
+            });
+
+            services.AddScoped<IDataSeeder, CatalogDataSeeder>();
 
             return services;
         }
 
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
+            //Use Api Endpoints Services
+
+            //Use Application Use Case Services
+
+            //Use Data - Infrastructure Services
+            
+            app.UseMigration<CatalogDbContext>();
 
             return app;
         }
