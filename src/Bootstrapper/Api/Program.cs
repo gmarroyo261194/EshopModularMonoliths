eeb1b449f7a118a373263564d08979cfdd366e3b
Guillermo Arroyo <gmarroyo@yahoo.com>
2024-11-19 16:56:17 -0300

    Se configuró Serilog para escribir en consola y enviar a SEQ. Se configuró docker-compose.yml para incluir la última version de la imagen de Seq

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((context, config) =>
+{
+    config.ReadFrom.Configuration(context.Configuration);
+});
+
 builder.Services
     .AddCarterWithAssemblies(typeof(CatalogModule).Assembly);
 
@@ -14,15 +19,12 @@ builder.Services
 var app = builder.Build();
 
 app.MapCarter();
+app.UseSerilogRequestLogging();
+app.UseExceptionHandler(options =>{});
 
 app
     .UseCatalogModule()
     .UseBasketModule()
     .UseOrderingModule();
-
-app.UseExceptionHandler(options =>
-{
-
-});
 
 app.Run();
